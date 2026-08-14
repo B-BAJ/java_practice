d1d7b361ffdfa3e97de1d90c163903073b1171d6
Blanka Bajuta <ethanotech5@gmail.com>
2026-08-15 00:14:15 +0300

    to check for prime program is ready for testing

--- ExamPrep/IsPrime.java
+++ ExamPrep/IsPrime.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 
 public class IsPrime {
+
+    public static boolean isPrime(int number) {
+
+    if (number <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i < number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        if(isPrime(number)){
+            System.out.println(number + "isprime number");
+        } else{
+            System.out.println(number + "the number is not prime");
+        }
+
 
 
     }
